2829dcf04548096482ab57e3fce4d7a29348af75
happyu <290945266@qq.com>
2019-02-18 22:49:34 +0800

    feat: 完成NavigationBar

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'category_page.dart';
+import 'cart_page.dart';
+import 'member_page.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  static final navigationBarColor = Colors.pink;
+
+  final List<BottomNavigationBarItem> navigationBarItems = [
+    BottomNavigationBarItem(
+        icon: Icon(
+          Icons.home,
+          color: navigationBarColor,
+        ),
+        title: Text(
+          '首页',
+          style: TextStyle(color: navigationBarColor),
+        )),
+    BottomNavigationBarItem(
+        icon: Icon(
+          Icons.category,
+          color: navigationBarColor,
+        ),
+        title: Text(
+          '分类',
+          style: TextStyle(color: navigationBarColor),
+        )),
+    BottomNavigationBarItem(
+        icon: Icon(
+          Icons.shopping_cart,
+          color: navigationBarColor,
+        ),
+        title: Text(
+          '购物车',
+          style: TextStyle(color: navigationBarColor),
+        )),
+    BottomNavigationBarItem(
+        icon: Icon(
+          Icons.person,
+          color: navigationBarColor,
+        ),
+        title: Text(
+          '会员中心',
+          style: TextStyle(color: navigationBarColor),
+        ))
+  ];
+
+  final List tabBodies = [HomePage(), CategoryPage(), CartPage(), MemberPage()];
+
+  int _currentIndex = 0;
+  var _currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPage = tabBodies[_currentIndex];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+        body: _currentPage,
+        bottomNavigationBar: BottomNavigationBar(
+          items: navigationBarItems,
+          // type: BottomNavigationBarType.fixed, // 0~3默认采用fixed模式, 4个采用shifting模式
+          currentIndex: _currentIndex,
+          onTap: handleOnTap,
+        ));
+  }
+
+  void handleOnTap(int index) {
+    setState(() {
+      _currentIndex = index;
+      _currentPage = tabBodies[index];
+    });
+  }
+}
